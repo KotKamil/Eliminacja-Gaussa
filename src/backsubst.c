86f622312b358c99c05f3b48305add65424fffcb
KotKamil <mariusz2054@gmail.com>
2024-12-13 15:09:31 +0100

    Added backsubst.c functionality and exception handling

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,18 +5,25 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	if (mat->c > mat->r 	||
+		b->r != mat->r		||
+		x->r != mat->c)
+		return 2;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	int i, j;
+	for (i = x->r-1; i >= 0; i--) {
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+		if (mat->data[i][i] == 0)
+			return 1;
 
-				return 0;
+		x->data[i][0] = b->data[i][0];
+		for (j = mat->c-1; j>i; j--){
+			x->data[i][0] -= x->data[j][0]*mat->data[i][j];
+		}
+		x->data[i][0] /= mat->data[i][i];
+	}
+
+	return 0;
 }
 
 
